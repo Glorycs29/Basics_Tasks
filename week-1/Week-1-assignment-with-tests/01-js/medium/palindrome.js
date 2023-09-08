ef24b26920eeb05597482aa9fece077b266ef0a4
Glory Gupta <prikarshglory@gmail.com>
2023-09-09 01:24:48 +0530

    Update palindrome.js

--- week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -6,8 +6,33 @@
   - `npm run test-palindrome`
 */
 
+function reverse(str){
+  var ans = "";
+  for(let i=str.length-1; i>=0; i--){
+    ans += str[i];
+  }
+  return ans;
+}
+
+function transform(str){
+  var ans = "";
+  for(let i=0; i<str.length; i++){
+    if(str[i]===" " || str[i]==="," || str[i]==="?" || str[i]==="!" || str[i]==="."){ 
+      continue;
+    }
+    else ans += str[i];
+  }
+  return ans;
+}
+
+
 function isPalindrome(str) {
-  return true;
+  str = str.toLowerCase();
+  str = transform(str);
+  let reverseStr = reverse(str);
+  
+  if(reverseStr === str) return true;
+  return false;
 }
 
 module.exports = isPalindrome;
